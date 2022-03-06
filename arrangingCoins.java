7e07f48b6a7ba97b80d91108c5ccb24ffbabc31b
Sonali Rajput <sonalirajput1088@gmail.com>
2022-03-06 11:10:29 +0530

    arranging coins BS

--- arrangingCoins.java
+++ arrangingCoins.java
@@ -9,15 +9,15 @@ public class arrangingCoins {
         int ans = 0;
 
         while(s<=e){
-            int m = s + (e-s)/2;
-            long sum = (long)m*(m+1)/2;
+            int m = s + (e-s)/2; 
+            long sum = (long)m*(m+1)/2; //total number of coins that can fit in that particular row.
             if(sum==n){
                 return m;
             }
-            if(n>=sum){
+            if(n>=sum){ //no. of coins is greater than total sum of coin then search in right.
                 s = m+1;
                 ans= m;
-            }else {
+            }else { //search in right
                 e = m-1;
             }
     }return ans;
