4d4b4db0933c9b4959f2916238abdfcbdcda6f78
Sonali Rajput <sonalirajput1088@gmail.com>
2022-03-16 23:07:14 +0530

    pattern 5

--- pattern5.java
+++ pattern5.java
@@ -9,17 +9,17 @@ public class pattern5 {
     }
     static void pattern11(int n) {
         for (int row = 1; row <=n; row++) {
-            for (int space = 0;space<n-row;space++) {
+            for (int space = 0;space<n-row;space++) { //for spaces
                 System.out.print(" ");
             }
-            for (int col = row;col>=1;col--) {
+            for (int col = row;col>=1;col--) { //for 21,321,4321..so on
                 System.out.print(col);
             }
 
-            for (int col = 2; col <=row; col++) {
+            for (int col = 2; col <=row; col++) {//for 2,23,234,2345..so on
                 System.out.print(col);
             }
-            System.out.println();
+            System.out.println(); //for new line
         }
 }
 }
