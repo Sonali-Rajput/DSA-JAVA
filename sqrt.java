fabfeb9e62676b2e349319ad82ddd32af292bf68
Sonali Rajput <sonalirajput1088@gmail.com>
2022-04-18 07:39:55 +0530

    binary search square root

--- sqrt.java
+++ sqrt.java
@@ -4,17 +4,13 @@ public class sqrt {
     public static void main(String[] args) {
         Scanner num = new Scanner(System.in);
         int x = num.nextInt();
-        System.out.println(Squart(x));
+        int p =3; //precision
+        System.out.printf("%.3f",binarySearch_sqrt(x,p));
         num.close();
-        
     }
-    static int Squart(int x){
-        if(x<=1)
-            return x;
-        return binarySearch(x);
-    }
-    static int binarySearch(int x){
-        int start = 1, end = x/2, mid, ans =0;
+    static double binarySearch_sqrt(int x,int p){
+        int start = 0, end = x, mid;
+        double root = 0.0;
         long temp;
         while(start<=end){
             mid = start + (end - start)/2;
@@ -22,17 +18,21 @@ public class sqrt {
             if(temp== x){
                 return mid;
             }
-            if(temp < x){
-                ans = mid;
-                start = mid + 1;
+            if(temp > x){
+                end = mid - 1;
             }
             else{
-                end = mid - 1;
-
+                start = mid + 1;
             }
-            //return (int)ans;
         }
-        return ans;
-        
+        double incr = 0.1;
+        for(int i=0;i<p;i++){
+            while(root*root<=x){
+                root += incr;
+            }
+            root -=incr;
+            incr/=10;
+        }
+        return root;
     }
 }
